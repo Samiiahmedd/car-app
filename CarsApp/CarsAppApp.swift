6a8a6b685197855e0af022189a575454a26098d3
Sami Ahmed <90336426+Samiiahmedd@users.noreply.github.com>
2023-10-11 18:46:36 +0300

    Initial Commit

---
+++ CarsApp/CarsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsAppApp.swift
+//  CarsApp
+//
+//  Created by Sami Ahmed on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarsApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CarsApp
+//
+//  Created by Sami Ahmed on 11/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
